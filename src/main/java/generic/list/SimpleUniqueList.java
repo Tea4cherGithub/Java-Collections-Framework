de81c5f9c51ede1ac967e434d6706325af82d4b5
Nikita <nik.nesterenko1999@bk.ru>
2022-09-08 11:49:43 +0300

    Задачи по дженерикам.

---
+++ src/main/java/generic/list/SimpleUniqueList.java
@@ -0,0 +1,58 @@
+package generic.list;
+
+import java.util.Iterator;
+
+public class SimpleUniqueList<T> implements ListContainer<T>{
+
+    private T[] array;
+
+    private int size;
+
+    private int modCount;
+
+    public SimpleUniqueList(int initialCapacity) {
+        this.array = (T[]) new Object[initialCapacity];
+    }
+
+    @Override
+    public void add(T value) {
+
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        return null;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public T next() {
+                return null;
+            }
+
+        };
+    }
+}
